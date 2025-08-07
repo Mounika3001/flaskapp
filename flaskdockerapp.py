d037e53a085cb86dbbdddbfdf7aebc0ae0b198a2
Mounika3001 <mounikam0471@gmail.com>
2025-08-07 12:55:34 +0530

    Fixed syntax error and updated message

--- flaskdockerapp.py
+++ flaskdockerapp.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
-0 return "Hello Docker this is the new update" 
-if __name__=="__main__":
-  app.run(host="0.0.0.0", port=5019)
+    return "Hello Docker this is the new update"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5019)
